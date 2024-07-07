3f547b8e3e9876a93da81a0584a018c2ce2f56cd
aditpee <p254881@gmail.com>
2024-07-07 16:43:47 +0700

    create middlewar for validaate input profile in server

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -12,6 +12,37 @@ const registerValidator = [
   body("username", "Username must be at least 4 chararters long").isLength({
     min: 4,
   }),
+  body("username", "Username maximal charater is 15").isLength({
+    max: 15,
+  }),
 ];
 
-module.exports = { registerValidator };
+const editProfileValidator = [
+  body(
+    "username",
+    "Your username can only contain letters, numbers and '_'"
+  ).matches(/^[a-zA-Z0-9_]+$/),
+  body("username", "Username must be at least 4 chararters long").isLength({
+    min: 4,
+  }),
+  body("username", "Username cannot have more than 15 characters").isLength({
+    max: 15,
+  }),
+  body("username", "Username can’t be blank").notEmpty(),
+  body("name", "Name can’t be blank").notEmpty(),
+  body("name", "Username cannot have more than 50 characters").isLength({
+    max: 50,
+  }),
+  body("bio", "Bio cannot have more than 160 characters").isLength({
+    max: 160,
+  }),
+  body("location", "Location cannot have more than 30 characters").isLength({
+    max: 30,
+  }),
+  body("website", "Website cannot have more than 100 characters").isLength({
+    max: 100,
+  }),
+  body("website", "Please input a valid url").isURL(),
+];
+
+module.exports = { registerValidator, editProfileValidator };
