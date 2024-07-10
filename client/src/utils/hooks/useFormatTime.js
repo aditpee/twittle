472ae0742aa5fe415ad1b46c676f3c7fe4a7fd48
aditpee <p254881@gmail.com>
2024-07-10 15:40:27 +0700

    trying to solve custuom hook render multiple time

--- client/src/utils/hooks/useFormatTime.js
+++ client/src/utils/hooks/useFormatTime.js
@@ -1,80 +1,97 @@
+import { useMemo } from "react";
+
 const useFormatTime = () => {
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+  const getMonth = useMemo(() => {
+    return (date) => {
+      const monthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      return monthNames[new Date(date).getMonth()];
+    };
+  }, []);
 
-  const getMonth = (date) => {
-    return monthNames[new Date(date).getMonth()];
-  };
-  const getYear = (date) => {
-    return new Date(date).getFullYear();
-  };
-  const getDate = (date) => {
-    return new Date(date).getDate();
-  };
+  const getYear = useMemo(() => {
+    return (date) => {
+      return new Date(date).getFullYear();
+    };
+  }, []);
 
-  const getTime = (date) => {
-    let hour = new Date(date).getHours();
-    const minute = new Date(date).getMinutes();
+  const getDate = useMemo(() => {
+    return (date) => {
+      return new Date(date).getDate();
+    };
+  }, []);
 
-    const pmAm = hour < 12 ? "AM" : "PM";
+  const getTime = useMemo(() => {
+    return (date) => {
+      let hour = new Date(date).getHours();
+      const minute = new Date(date).getMinutes();
 
-    if (hour === 0) hour = 12;
+      const pmAm = hour < 12 ? "AM" : "PM";
 
-    return `${hour}:${minute} ${pmAm}`;
-  };
+      if (hour === 0) hour = 12;
 
-  const getFullDate = (date) => {
-    const day = getDate(date);
-    const month = getMonth(date);
-    const year = getYear(date);
+      return `${hour}:${minute} ${pmAm}`;
+    };
+  }, []);
 
-    return `${month} ${day}, ${year}`;
-  };
+  const getFullDate = useMemo(() => {
+    return (date) => {
+      const day = getDate(date);
+      const month = getMonth(date);
+      const year = getYear(date);
 
-  function timeAgo(date) {
-    const seconds = Math.floor((new Date() - date) / 1000);
-
-    let interval = seconds / 31536000;
-    const month = monthNames[new Date(date).getMonth()];
-    const year = new Date(date).getFullYear();
-    const day = new Date(date).getDate();
-
-    if (interval > 1) {
       return `${month} ${day}, ${year}`;
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return `${month} ${day}`;
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return `${month} ${day}`;
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + "h";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + "m";
-    }
-    if (seconds !== 0) {
-      return Math.floor(seconds) + "s";
-    }
-    return "Now";
-  }
+    };
+  }, [getDate, getMonth, getYear]);
+
+  const timeAgo = useMemo(() => {
+    return (date) => {
+      const seconds = Math.floor((new Date() - date) / 1000);
+
+      let interval = seconds / 31536000;
+      const month = getMonth(date);
+      const year = new Date(date).getFullYear();
+      const day = new Date(date).getDate();
+      let kkdk = 0;
+      console.log(kkdk++);
+
+      if (interval > 1) {
+        return `${month} ${day}, ${year}`;
+      }
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return `${month} ${day}`;
+      }
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return `${month} ${day}`;
+      }
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + "h";
+      }
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + "m";
+      }
+      if (seconds !== 0) {
+        return Math.floor(seconds) + "s";
+      }
+      return "Now";
+    };
+  }, [getMonth]);
 
   return { timeAgo, getMonth, getYear, getTime, getFullDate };
 };
