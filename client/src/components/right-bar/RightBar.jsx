cb4768a7b530c4d9c578d02cf6b7b8ed2a50b485
aditpee <p254881@gmail.com>
2024-07-12 03:27:35 +0700

    implemented api get random user at right bar component

--- client/src/components/right-bar/RightBar.jsx
+++ client/src/components/right-bar/RightBar.jsx
@@ -1,7 +1,12 @@
 import { MoreHoriz } from "@mui/icons-material";
+import axios from "axios";
+import { useEffect } from "react";
 import { useRef } from "react";
+import { useContext } from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { API_URL, PF } from "../../config";
+import { AuthContext } from "../../context/AuthContext";
 import { SearchOutline } from "../../utils/icons/icons";
 import SearchBar from "../search-bar/SearchBar";
 import "./right-bar.scss";
@@ -10,6 +15,28 @@ const RightBar = () => {
   const [searchValue, setSearchValue] = useState("");
   const searchRef = useRef(null);
   const { pathname } = useLocation();
+  const [users, setUsers] = useState([]);
+
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(API_URL + "/api/users?limit=2", {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        setUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [token]);
+
+  console.log(users);
 
   return (
     <section className="right-bar">
@@ -22,60 +49,33 @@ const RightBar = () => {
         <div className="right-bar-card">
           <div className="flow">
             <h4 className="fs-700">You might like</h4>
-            <div className="right-bar-follow-card d-flex align-center">
-              <div className="d-flex align-center">
-                <div className="right-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
+            {users.map((user) => (
+              <div
+                key={user._id}
+                className="right-bar-follow-card d-flex align-center"
+              >
+                <div className="">
+                  <div className="right-bar-avatar margin-inline-end-3">
+                    <img
+                      className="radius-circle"
+                      src={
+                        user?.avatar
+                          ? user?.avatar
+                          : PF + "/images/no-avatar.svg"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <p className="fs-400 fw-bold">{user?.name}</p>
+                    <p className="fs-300 clr-neutral-600">{`@${user?.username}`}</p>
+                  </div>
                 </div>
-                <div>
-                  <p className="fs-400 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
-                </div>
+                <button className="fs-300 fw-bold bg-neutral-800 clr-neutral-000 padding-inline-4 padding-block-2 radius-2">
+                  Following
+                </button>
               </div>
-              <button className="fs-300 fw-bold bg-neutral-800 clr-neutral-000 padding-inline-4 padding-block-2 radius-2">
-                Following
-              </button>
-            </div>
-            <div className="right-bar-follow-card d-flex align-center">
-              <div className="d-flex align-center">
-                <div className="right-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <p className="fs-400 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
-                </div>
-              </div>
-              <button className="fs-300 fw-bold bg-neutral-800 clr-neutral-000 padding-inline-4 padding-block-2 radius-2">
-                Follow
-              </button>
-            </div>
-            <div className="right-bar-follow-card d-flex align-center">
-              <div className="d-flex align-center">
-                <div className="right-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <p className="fs-400 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
-                </div>
-              </div>
-              <button className="fs-300 fw-bold bg-neutral-800 clr-neutral-000 padding-inline-4 padding-block-2 radius-2">
-                Follow
-              </button>
-            </div>
+            ))}
             <p className="fs-200 clr-primary-000">Show more</p>
           </div>
         </div>
