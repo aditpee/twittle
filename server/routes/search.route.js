3816b1252df376942c16e845eb9fe1e873be0377
aditpee <p254881@gmail.com>
2024-07-17 15:46:32 +0700

    create api for serach post, user, and post have image

--- server/routes/search.route.js
+++ server/routes/search.route.js
@@ -27,6 +27,68 @@ router.get("/", verifyJwt, async (req, res) => {
 
     res.status(200).json(user);
   } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// search post by query
+router.get("/post/", verifyJwt, async (req, res) => {
+  try {
+    const posts = await Post.aggregate([
+      {
+        $match: { text: { $regex: `${req.query.q}`, $options: "mi" } },
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+    ]);
+
+    res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/media/", verifyJwt, async (req, res) => {
+  try {
+    const posts = await Post.aggregate([
+      {
+        $match: {
+          text: { $regex: `${req.query.q}`, $options: "mi" },
+          image: { $ne: "" },
+        },
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+    ]);
+
+    res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// search people by query
+router.get("/people/", verifyJwt, async (req, res) => {
+  try {
+    const users = await User.aggregate([
+      {
+        $match: {
+          verifiedEmail: true,
+          $or: [
+            { name: { $regex: `${req.query.q}`, $options: "mi" } },
+            { username: { $regex: `${req.query.q}`, $options: "i" } },
+          ],
+        },
+      },
+      { $sort: { verifiedAccount: -1 } },
+    ]);
+
+    res.status(200).json(users);
+  } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal server error" });
   }
