db339c7dfed8a9f672b973bd5dee929fa3ed289d
aditpee <p254881@gmail.com>
2024-07-17 15:04:59 +0700

    implement api search automple in search bar component, and navigate to profile if we clicked

--- client/src/components/search-bar/SearchBar.jsx
+++ client/src/components/search-bar/SearchBar.jsx
@@ -4,22 +4,58 @@ import { useState } from "react";
 import { useRef } from "react";
 import useOutsideClick from "../../utils/hooks/useOutSideClick";
 import { SearchOutline } from "../../utils/icons/icons";
+import axios from "axios";
 import "./search-bar.scss";
+import { API_URL, PF } from "../../config";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchBar = ({ setIsSearchClicked }) => {
   const [searchValue, setSearchValue] = useState("");
   const searchRef = useRef(null);
   const autoCompleteRef = useRef(null);
+  const { token } = useContext(AuthContext);
+  const [searchUsers, setSearchUsers] = useState([]);
+  const navigate = useNavigate();
+
+  const debounced = useDebouncedCallback((e, fnCallback) => {
+    fnCallback(e);
+  }, 500);
+
+  const handleNavigateSearch = (url) => {
+    navigate(`/${url}`);
+  };
 
   const handleClickOutside = () => {
     autoCompleteRef.current.classList.add("hide");
     setIsSearchClicked && setIsSearchClicked(false);
   };
   const handleClickInside = () => {
-    autoCompleteRef.current.classList.remove("hide");
+    autoCompleteRef.current && autoCompleteRef.current.classList.remove("hide");
     setIsSearchClicked && setIsSearchClicked(true);
   };
   const searchBarRef = useOutsideClick(handleClickOutside, handleClickInside);
+
+  const handleSearch = async (e) => {
+    if (!e.target.value) return;
+    try {
+      const res = await axios.get(
+        API_URL + "/api/search?people=" + e.target.value,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      console.log(res.data);
+      setSearchUsers(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="search-bar" ref={searchBarRef}>
@@ -35,6 +71,10 @@ const SearchBar = ({ setIsSearchClicked }) => {
             ref={searchRef}
             value={searchValue}
             onInput={(e) => setSearchValue(e.target.value)}
+            onChange={(e) => {
+              setSearchUsers([]);
+              debounced(e, handleSearch);
+            }}
           />
         </div>
         <div
@@ -44,11 +84,11 @@ const SearchBar = ({ setIsSearchClicked }) => {
             searchRef.current.focus();
           }}
         >
-          {searchValue ? (
-            <Cancel onClick={() => setIsSearchClicked(true)} />
-          ) : (
-            ""
-          )}
+          <div
+            className={`remove-search-icon d-flex ${searchValue ? "" : "hide"}`}
+          >
+            <Cancel hidden onClick={() => setIsSearchClicked(true)} />
+          </div>
         </div>
       </div>
       <div
@@ -60,51 +100,33 @@ const SearchBar = ({ setIsSearchClicked }) => {
             <div className="search-bar-autocomplete-header clr-neutral-600">
               <p>Search for {searchValue}</p>
             </div>
-            <div className="search-bar-autocomplete-card">
-              <div className="d-flex align-center">
-                <div className="search-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <p className="fs-400 clr-neutral-800 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
-                </div>
-              </div>
-            </div>
-            <div className="search-bar-autocomplete-card">
-              <div className="d-flex align-center">
-                <div className="search-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <p className="fs-400 clr-neutral-800 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
+            {searchUsers.map((user) => (
+              <div
+                key={user._id}
+                onClick={() => handleNavigateSearch(user?.username)}
+                className="search-bar-autocomplete-card"
+              >
+                <div className="d-flex align-center">
+                  <div className="search-bar-avatar margin-inline-end-3">
+                    <img
+                      className="radius-circle"
+                      src={
+                        user?.avatar
+                          ? user?.avatar
+                          : PF + "/images/no-avatar.svg"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <p className="fs-400 clr-neutral-800 fw-bold">
+                      {user?.name}
+                    </p>
+                    <p className="fs-300 clr-neutral-600">{`@${user?.username}`}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="search-bar-autocomplete-card">
-              <div className="d-flex align-center">
-                <div className="search-bar-avatar margin-inline-end-3">
-                  <img
-                    className="radius-circle"
-                    src="./images/no-avatar.svg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <p className="fs-400 clr-neutral-800 fw-bold">Name Twittle</p>
-                  <p className="fs-300 clr-neutral-600">@username</p>
-                </div>
-              </div>
-            </div>
+            ))}
             <p className="fs-200 clr-neutral-800">{"go to @" + searchValue}</p>
           </>
         ) : (
