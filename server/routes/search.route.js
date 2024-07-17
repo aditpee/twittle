ffc4c82daa72b79c3b5284d5465927c47cc87e72
aditpee <p254881@gmail.com>
2024-07-17 15:01:45 +0700

    make search autocomplete work if we search name, not only search username

--- server/routes/search.route.js
+++ server/routes/search.route.js
@@ -9,10 +9,21 @@ const verifyJwt = require("../middleware/authMiddleware");
 
 router.get("/", verifyJwt, async (req, res) => {
   try {
-    console.log(req.query.people);
-    const user = await User.find({
-      username: { $regex: `${req.query.people}`, $options: "i" },
-    });
+    // const user = await User.find({
+    //   username: { $regex: `${req.query.people}`, $options: "i" },
+    // });
+    const user = await User.aggregate([
+      {
+        $match: {
+          verifiedEmail: true,
+          $or: [
+            { name: { $regex: `${req.query.people}`, $options: "mi" } },
+            { username: { $regex: `${req.query.people}`, $options: "i" } },
+          ],
+        },
+      },
+      { $limit: 10 },
+    ]);
 
     res.status(200).json(user);
   } catch (err) {
